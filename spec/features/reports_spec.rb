a7317bbc73fadc6b5cfdc45d19a9d6d146e0b0fc
Doug <djmacdonald@uchicago.edu>
2015-02-12 00:30:12 -0500

    Routing tests

--- spec/features/reports_spec.rb
+++ spec/features/reports_spec.rb
@@ -8,11 +8,11 @@ RSpec.feature 'Managing reports' do
     Report.create!(name:'John Smith', state: 'New York', city:'New York', incident_date:'2004-02-02', agency_id: fbi.id, user_id: user.id, verified: true, archived: false)
     Report.create!(name:'John Smith', state: 'New York', city:'New York', incident_date:'2004-02-02', agency_id: fbi.id, user_id: user.id, verified: true, archived: false)
     Report.create!(name:'John Smith', state: 'New York', city:'New York', incident_date:'2004-02-02', agency_id: fbi.id, user_id: user.id, verified: true, archived: false)
-    Report.create!(name:'John Smith', state: 'New York', city:'New York', incident_date:'2004-02-02', agency_id: fbi.id, user_id: user.id, verified: true, archived: false)
-    Report.create!(name:'John Smith', state: 'New York', city:'New York', incident_date:'2004-02-02', agency_id: fbi.id, user_id: user.id, verified: true, archived: false)
-    Report.create!(name:'John Smith', state: 'New York', city:'New York', incident_date:'2004-02-02', agency_id: fbi.id, user_id: user.id, verified: true, archived: false)
-    Report.create!(name:'John Smith', state: 'New York', city:'New York', incident_date:'2004-02-02', agency_id: fbi.id, user_id: user.id, verified: true, archived: false)
-    Report.create!(name:'John Smith', state: 'New York', city:'New York', incident_date:'2004-02-02', agency_id: fbi.id, user_id: user.id, verified: true, archived: false)
+    Report.create!(name:'John Smith', state: 'New York', city:'New York', incident_date:'2004-02-02', agency_id: dea.id, user_id: user.id, verified: true, archived: false)
+    Report.create!(name:'John Smith', state: 'Maine', city:'New York', incident_date:'2004-02-02', agency_id: dea.id, user_id: user.id, verified: true, archived: false)
+    Report.create!(name:'John Smith', state: 'Maine', city:'New York', incident_date:'2004-02-02', agency_id: dea.id, user_id: user.id, verified: true, archived: false)
+    Report.create!(name:'John Smith', state: 'Maine', city:'New York', incident_date:'2004-02-02', agency_id: dea.id, user_id: user.id, verified: true, archived: false)
+    Report.create!(name:'John Smith', state: 'Maine', city:'New York', incident_date:'2004-02-02', agency_id: fbi.id, user_id: user.id, verified: true, archived: false)
   end
   scenario 'List all verified reports' do
     visit '/reports'
@@ -24,7 +24,20 @@ RSpec.feature 'Managing reports' do
     visit 'reports'
     select('FBI', from: 'agency_name')
     click_on 'Filter'
-    expect(page).to have_selector '.panel', count: 8
+    expect(page).to have_selector '.panel', count: 4
+  end
+  scenario 'Filter by State' do
+    visit 'reports'
+    select('New York', from: 'state_name')
+    click_on 'Filter'
+    expect(page).to have_selector '.panel', count: 4
+  end
+  scenario 'Filter by Agency and State' do
+    visit 'reports'
+    select('FBI', from: 'agency_name')
+    select('New York', from: 'state_name')
+    click_on 'Filter'
+    expect(page).to have_selector '.panel', count: 3
   end
   scenario 'Create a new report' do
     visit 'reports/new'
--- spec/routing/reports_routing_spec.rb
+++ spec/routing/reports_routing_spec.rb
@@ -1,6 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe 'routes for reports' do
+  it 'routes GET /users/sign_in to the devise controller' do
+    expect(get('/users/sign_in')).to route_to('devise/sessions#new')
+  end
+  it 'routes Post /users/sign_in to the create action' do
+    expect(post('/users/sign_in')).to route_to('devise/sessions#create')
+  end
+  it 'routes DELETE /users/sign_out to the destroy action' do
+    expect(delete('/users/sign_out')).to route_to('devise/sessions#destroy')
+  end
+  it 'routes GET / to the welcome controller' do
+    expect(get('/')).to route_to('welcome#index')
+  end
+  it 'routes POST /agencies to the create action' do
+    expect(post('/agencies')).to route_to('agencies#create')
+  end
+    it 'routes GET /agencies/1 to the agencies controller and sets id' do
+    expect(get('/agencies/1')).to route_to(controller: 'agencies', action: 'show', id: '1')
+  end
   it 'routes GET /reports to the reports controller' do
     expect(get('/reports')).to route_to('reports#index')
   end
@@ -10,4 +28,15 @@ RSpec.describe 'routes for reports' do
   it 'routes POST /reports to the create action' do
     expect(post('/reports')).to route_to('reports#create')
   end
+  it 'routes GET /reports/check to the reports controller' do
+    expect(get('/reports/check')).to route_to('reports#check')
+  end
+  it 'routes GET /users/1 to the users controller and sets id' do
+    expect(get('/users/1')).to route_to(controller: 'users', action: 'show', id: '1')
+  end
+  it 'routes anything else to the root path' do
+    expect(get('/bmsdfd')).to route_to(controller: 'welcome', action: 'index', path: 'bmsdfd')
+  end
+
+
 end
