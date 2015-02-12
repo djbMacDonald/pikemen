dce8a91b6b8b7ccae7b4c25ecce5bf68e4aa02a9
Doug <djmacdonald@uchicago.edu>
2015-02-12 08:54:30 -0500

    Cleaned up routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'welcome#index'
-  resources :agencies do
-    resources :reports, except: :show
-  end
+  resources :agencies, except: [:index, :edit, :update, :destroy]
   get '/reports/check', to: 'reports#check'
   resources :reports, except: :show
   get '/users/:id', to: 'users#show', as: 'user'
